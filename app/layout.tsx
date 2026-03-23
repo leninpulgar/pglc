eae2459317647b49996a909795ed216126391e31
Lenin <lenindpulgar@gmail.com>
2026-03-23 15:50:30 -0500

    pglc-minor change

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "2026 Inspiring Leaders",
-  description: "Celebrate 2026 Inspiring Leaders with a personalized badge."
+  description: "Celebrate 2026 PGLC with a personalized badge."
 };
 
 export default function RootLayout({
@@ -27,16 +27,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');` }} />
+        })(window,document,'script','dataLayer','${GTM_ID}');`
+        }} />
       </head>
       <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-        <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`} height="0" width="0" style={{display: 'none', visibility: 'hidden'}}></iframe></noscript>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`} height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         {children}
       </body>
     </html>
